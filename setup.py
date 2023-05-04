5dceb461e396556e321dcf3470a280faf4abe8ae
Yanina Kutovaya <kutovaiayp@yandex.ru>
2023-05-04 14:04:30 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name="predictive-maintenance",
+    version="1.0",
+    description="LSTM for predictive maintenance",
+    author="Yanina Kutovaya",
+    author_email="kutovaiayp@yandex.ru",
+    url="https://github.com/Yanina-Kutovaya/predictive-maintenance.git",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+)
