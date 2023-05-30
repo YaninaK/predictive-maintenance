1033588b6dcc969820134fdb399dff5db50e5958
Yanina Kutovaya <kutovaiayp@yandex.ru>
2023-05-30 22:25:54 +0300

    Fix make_dataset.py

--- src/predictive_maintenance/data/make_dataset.py
+++ src/predictive_maintenance/data/make_dataset.py
@@ -71,23 +71,23 @@ def load_data(
     y_train = spark.read.parquet(y_train_path, header=True, inferSchema=True)
     messages = pd.read_excel(messages_path, index_col=0)
 
-    unified_tech_places = get_unified_tech_places(
-        y_train, save, path, folder_2, unified_tech_places_path
-    )
-    messages = add_unified_names_to_messages(
-        messages, unified_tech_places, save, path, folder_2, messages_unified_path
-    )
-
     X_cols = get_new_X_column_names(X_train)
     X_train = rename_columns(X_train, X_cols)
     y_cols = get_new_y_column_names(y_train)
     y_train = rename_columns(y_train, y_cols)
 
+    unified_tech_places = get_unified_tech_places(
+        y_cols, save, path, folder_2, unified_tech_places_path
+    )
+    messages = add_unified_names_to_messages(
+        messages, unified_tech_places, save, path, folder_2, messages_unified_path
+    )
+
     return X_train, y_train, messages, unified_tech_places
 
 
 def get_unified_tech_places(
-    y_train,
+    y_cols: list,
     save: bool,
     path: str,
     folder: str,
@@ -96,34 +96,34 @@ def get_unified_tech_places(
     """
     Unifies technical places names to enable equipment comparison.
     """
-    tech_places = y_train.schema.names[1:]
-    eq = [i.split("_")[1][-1] for i in tech_places]
-    desc = [i.split("_")[2] for i in tech_places]
+    tech_places = y_cols[1:]
+    eq = [i[0] for i in tech_places]
+    desc = [i for i in tech_places]
 
     df = pd.DataFrame(zip(eq, desc), columns=["equipment", "description"])
     for i, name in enumerate(desc):
         for j in range(4, 10):
-            if (name[7] == str(j)) & (name[-1] == str(j)):
-                df.loc[i, "unified_name"] = name[:7] + name[8:-1]
+            if (name[9] == str(j)) & (name[-1] == str(j)):
+                df.loc[i, "unified_name"] = name[2:9] + name[10:-1]
             elif name[-5] == str(j):
-                df.loc[i, "unified_name"] = name[:-5] + name[-4:]
-            elif name[:6] == "САПФИР":
-                df.loc[i, "unified_name"] = name
-            elif name[:19] == "ПОДШИПНИК ОПОРНЫЙ №":
-                df.loc[i, "unified_name"] = name[:20]
-            elif name[:-1] in [
-                "ТСМТ-101-010-50М-400 ТЕРМОПР.ПОДШ.Т.",
-                "ТСМТ-101-010-50М-200 ТЕРМОПР.ПОДШ.Т.",
-                "ТСМТ-101-010-50М-80 ТЕРМОПРЕОБР.МАСЛ",
+                df.loc[i, "unified_name"] = name[2:-5] + name[-4:]
+            elif name[2:8] == "САПФИР":
+                df.loc[i, "unified_name"] = name[2:]
+            elif name[2:21] == "ПОДШИПНИК ОПОРНЫЙ №":
+                df.loc[i, "unified_name"] = name[2:22]
+            elif name[2:-1] in [
+                "ТСМТ-101-010-50М-400 ТЕРМОПР_ПОДШ_Т_",
+                "ТСМТ-101-010-50М-200 ТЕРМОПР_ПОДШ_Т_",
+                "ТСМТ-101-010-50М-80 ТЕРМОПРЕОБР_МАСЛ",
                 "ТИРИСТОРНЫЙ ВОЗБУДИТЕЛЬ СПВД-М10-400-",
             ]:
-                df.loc[i, "unified_name"] = name
-            elif name[:18] == "МАСЛОПРОВОДЫ ЭКСГ ":
+                df.loc[i, "unified_name"] = name[2:]
+            elif name[2:20] == "МАСЛОПРОВОДЫ ЭКСГ ":
                 df.loc[i, "unified_name"] = "МАСЛОПРОВОДЫ ЭКСГАУСТЕРА №"
-            elif name[:24] == "ЭЛЕКТРООБОРУДОВАНИЯ ЭКСГ":
+            elif name[2:26] == "ЭЛЕКТРООБОРУДОВАНИЯ ЭКСГ":
                 df.loc[i, "unified_name"] = "ЭЛЕКТРООБОРУДОВАНИЯ ЭКСГАУСТЕРА №"
             elif name[-1] == str(j):
-                df.loc[i, "unified_name"] = name[:-1]
+                df.loc[i, "unified_name"] = name[2:-1]
     if save:
         df.to_parquet(unified_tech_places_path)
 
@@ -142,12 +142,15 @@ def add_unified_names_to_messages(
     Adds unified technical place names and equipment references to messages
     to match messages to y_train data.
     """
-    desc = unified_tech_places["description"].tolist()
+    desc = [i[2:] for i in unified_tech_places["description"]]
     unified_desc = unified_tech_places["unified_name"].tolist()
     dict_ = {desc[i]: unified_desc[i] for i in range(len(unified_desc))}
 
     for i in messages.index:
         original_name = messages.loc[i, "НАЗВАНИЕ_ТЕХ_МЕСТА"]
+        original_name = re.sub("\(", "", original_name)
+        original_name = re.sub("\)", "", original_name)
+        original_name = re.sub("\.", "_", original_name)
         messages.loc[i, "equipment"] = messages.loc[i, "ИМЯ_МАШИНЫ"][-1]
         messages.loc[i, "unified_name"] = dict_[original_name]
     if save:
