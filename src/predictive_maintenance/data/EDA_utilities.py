1ae1d08ff94cf893366179612a70ffd4e627e272
Yanina Kutovaya <kutovaiayp@yandex.ru>
2023-06-01 12:46:10 +0300

    Update EDA_utilities.py

--- src/predictive_maintenance/data/EDA_utilities.py
+++ src/predictive_maintenance/data/EDA_utilities.py
@@ -1,14 +1,7 @@
 import logging
 import pandas as pd
 import numpy as np
-import re
-import nltk
-from nltk.corpus import stopwords
-from sklearn.feature_extraction.text import TfidfVectorizer
-from pymystem3 import Mystem
 from typing import Optional
-
-nltk.download("stopwords")
 
 
 logger = logging.getLogger(__name__)
@@ -16,172 +9,17 @@ logger = logging.getLogger(__name__)
 __all__ = ["EDA_utilities"]
 
 PATH = ""
-FOLDER = "data/02_intermediate/"
+FOLDER_1 = "data/02_intermediate/"
+FOLDER_2 = "data/03_primary/"
 
-GROUPS = {
-    "oil_system": [
-        "ГСМ ЭКСГ. №",
-        "ДВИГАТЕЛЬ ПУСКОВОГО МАСЛОНАСОСА ЭКСГ. №",
-        "ДВИГАТЕЛЬ РЕЗЕРВНОГО МАСЛОНАСОСА ЭКСГ.№",
-        "ЗАПОРНАЯ АРМАТУРА ЭКСГАУСТЕРА №",
-        "КОЖУХ МУФТЫ ЭКСГ. №",
-        "МАСЛОБАК ЭКСГ. №",
-        "МАСЛОНАСОС РАБОЧИЙ ЭКСГ. №",
-        "МАСЛОНАСОС ШЕСТЕРЕНЧАТЫЙ (ПУСК.) ЭКСГ.№",
-        "МАСЛОНАСОС ШЕСТЕРЕНЧАТЫЙ (РЕЗ.) ЭКСГ. №",
-        "МАСЛООХЛАДИТЕЛЬ  М-05-1 ЭКСГ. №",
-        "МАСЛОПРОВОДЫ ЭКСГАУСТЕРА №",
-        "МАСЛОСТАНЦИЯ ЖИДКОЙ СМАЗКИ ЭКСГ. №",
-        "МАСЛЯНЫЙ ФИЛЬТР ЭКСГАУСТЕРА №",
-        "МЕТРАН-100 ДАТЧИКИ ДАВЛЕНИЯ ЭКСГ.№",
-        "ТСМТ-101-010-50М-80 ТЕРМОПРЕОБР.МАСЛО",
-    ],
-    "bearings": [
-        "ВК 310С ВИБРОПРЕОБРАЗОВАТЕЛЬ ЭКСГ.№ Т.1",
-        "ВК 310С ВИБРОПРЕОБРАЗОВАТЕЛЬ ЭКСГ.№ Т.2",
-        "ВК 310С ВИБРОПРЕОБРАЗОВАТЕЛЬ ЭКСГ.№ Т.3",
-        "ВК 310С ВИБРОПРЕОБРАЗОВАТЕЛЬ ЭКСГ.№ Т.4",
-        "ПОДШИПНИК ОПОРНО-УПОРНЫЙ ЭКСГ. №",
-        "ПОДШИПНИК ОПОРНЫЙ ЭКСГ. №",
-        "ПОДШИПНИК ОПОРНЫЙ №1",
-        "ПОДШИПНИК ОПОРНЫЙ №2",
-        "РОТОР ЭКСГ. №",
-        "ТСМТ-101-010-50М-400 ТЕРМОПР.ПОДШ.Т.1",
-        "ТСМТ-101-010-50М-400 ТЕРМОПР.ПОДШ.Т.2",
-        "ТСМТ-101-010-50М-200 ТЕРМОПР.ПОДШ.Т.3",
-        "ТСМТ-101-010-50М-200 ТЕРМОПР.ПОДШ.Т.4",
-    ],
-    "electric_devices": [
-        "КЛ1 ТР№ ДО ЭД ЭКСГАУСТЕРА №",
-        "КЛ2 ТР№ ДО ЭД ЭКСГАУСТЕРА №",
-        "ТИРИСТОРНЫЙ ВОЗБУДИТЕЛЬ СПВД-М10-400-5",
-        "ТИРИСТОРНЫЙ ВОЗБУДИТЕЛЬ ТВ-400 ЭКСГ ВУ1",
-        "ТИРИСТ. ВОЗБУДИТЕЛЬ ВТ-РЭМ-400 ЭКСГ ВУ1",
-        "ТИРИСТ. ВОЗБУДИТЕЛЬ ВТ-РЭМ-400 ЭКСГ ВУ2",
-        "ЭЛЕКТРОАППАРАТУРА ЭКСГ. №",
-        "ЭЛЕКТРОДВИГАТЕЛЬ ДСПУ-140-84-4 ЭКСГ. №",
-    ],
-    "fittings": [
-        "ГАЗОВАЯ ЗАДВИЖКА ЭКСГАУСТЕРА А/М №",
-        "ЗАДВИЖКА ЭКСГ. №",
-        "ЗАП. И РЕГ. АРМАТУРА ЭКСГ.№",
-        "КОРПУС ЭКСГ. №",
-        "РЕДУКТОР ГАЗ. ЗАДВИЖКИ ЭКСГ. №",
-        "РЕГУЛИРУЮЩАЯ АППАРАТУРА ЭКСГАУСТЕРА №",
-        "САПФИР 22 МДД ПЕРЕПАД ДАВЛ. НА ЦИКЛОНЕ",
-        "САПФИР 22 МДД РАЗРЕЖЕНИЕ В КОЛЛЕКТОРЕ",
-        "ТР-Р ТМ-4000-10/6 ЭКСГ. №",
-        "ТР-Р ТМ-6300-10/6 ЭКСГ. №",
-        "УЛИТА ЭКСГ. №",
-        "ЭЛ/ДВИГАТЕЛЬ ГАЗ. ЗАДВИЖКИ ЭКСГ. №",
-        "ЭЛЕКТРООБОРУДОВАНИЯ ЭКСГАУСТЕРА №",
-    ],
-    "other": ["ЭКСГАУСТЕР А/М №", "ЭКСГАУСТЕР Н-8000 А/М №"],
-}
-MAX_FEATURES = 150
-
-
-def add_groups_to_messages(messages, groups: Optional[dict] = None):
-    """
-    Classifies equipment into larger groups.
-    """
-    if groups is None:
-        groups = GROUPS
-
-    for group in groups.keys():
-        messages.loc[messages["unified_name"].isin(groups[group]), "groups"] = group
-
-    return messages
-
-
-def identify_lack_of_messages_in_y_train(
-    messages: pd.DataFrame,
-    unified_tech_places: pd.DataFrame,
-):
-    """
-    Identifies equipment failures and anomalies in y_train without
-    description in messages.
-    """
-    y_spec = pd.pivot_table(
-        unified_tech_places,
-        index="unified_name",
-        columns="equipment",
-        values="description",
-        aggfunc="count",
-        margins=True,
-    )
-    y_spec.sort_index(inplace=True)
-
-    no_messages = list(
-        set(unified_tech_places["unified_name"]) - set(messages["unified_name"])
-    )
-    messages_spec = pd.pivot_table(
-        messages,
-        index="unified_name",
-        columns="equipment",
-        values="ОПИСАНИЕ",
-        aggfunc="count",
-        margins=True,
-    )
-    for tech_place in no_messages:
-        messages_spec.loc[tech_place, :] = np.nan
-    messages_spec.sort_index(inplace=True)
-
-    a = np.where(y_spec.values > 0, 1, 0)
-    b = np.where(messages_spec.values > 0, 1, 0)
-    missing_messages = pd.DataFrame(
-        (a - b)[1:, :-1], index=y_spec.index[1:], columns=y_spec.columns[:-1]
-    )
-    missing_messages.loc[:, "All"] = (a - b)[1:, :-1].sum(axis=1)
-    missing_messages = missing_messages[missing_messages["All"] > 0]
-
-    return messages_spec, missing_messages
-
-
-def get_vectorizer_and_messages_vectors(
-    messages: pd.DataFrame, max_features: Optional[int] = None
-):
-    """
-    Vectorizes messages and returns fitted vectorizer with
-    sparse matrix representing descriptions in messages.
-    """
-    if max_features is None:
-        max_features = MAX_FEATURES
-
-    morph = Mystem()
-
-    def preprocess_text(text):
-        return " ".join(morph.lemmatize(text))
-
-    text = messages["ОПИСАНИЕ"].apply(preprocess_text)
-
-    stop_words = [
-        "эксг.",
-        "эк",
-        "co",
-        "6кв",
-        "ввд",
-        "№",
-        "2ру",
-        "22",
-        "40",
-        "\.",
-    ]
-
-    params = {
-        "stop_words": stopwords.words("russian") + stop_words,
-        "ngram_range": (1, 2),
-        "max_df": 0.9,
-        "max_features": max_features,
-    }
-    vectorizer = TfidfVectorizer(**params)
-    X = vectorizer.fit_transform(text)
-
-    return vectorizer, X
+POSTFIX = "resampled"
 
 
 def load_y(
-    i: int, path: Optional[str] = None, folder: Optional[str] = None
+    i: int,
+    path: Optional[str] = None,
+    folder: Optional[str] = None,
+    postfix: Optional[str] = None,
 ) -> pd.DataFrame:
     """
     Uploads resampled y_train.
@@ -189,9 +27,11 @@ def load_y(
     if path is None:
         path = PATH
     if folder is None:
-        folder = FOLDER
+        folder = FOLDER_1
+    if postfix is None:
+        postfix = POSTFIX
 
-    y = pd.read_parquet(path + folder + f"y{i}_resampled.parquet").drop(
+    y = pd.read_parquet(path + folder + f"y{i}_{postfix}.parquet").drop(
         "max(epoch)", axis=1
     )
     y.columns = ["dt"] + [i[4:-1] for i in y.columns[1:].tolist()]
@@ -228,13 +68,13 @@ def get_anomaly_dict(y: pd.DataFrame, t: pd.Timedelta) -> dict:
 
 def get_description_dictionary(unified_tech_places: pd.DataFrame) -> dict:
     """
-    Generates dictionary {description: unified_name} for y_train summary.
+    Generates dictionary {description: unified_name}.
     """
     description = unified_tech_places["description"].tolist()
     unified_name = unified_tech_places["unified_name"].tolist()
-    description_dict = {k: v for (k, v) in zip(description, unified_name)}
+    description_dictionary = {k: v for (k, v) in zip(description, unified_name)}
 
-    return description_dict
+    return description_dictionary
 
 
 def get_y_summary(
@@ -248,9 +88,9 @@ def get_y_summary(
     if path is None:
         path = PATH
     if folder is None:
-        folder = FOLDER
+        folder = FOLDER_1
 
-    description_dict = get_description_dictionary(unified_tech_places)
+    description_dictionary = get_description_dictionary(unified_tech_places)
     t = pd.Timedelta("1T")
     n = 0
     df = pd.DataFrame()
@@ -263,7 +103,92 @@ def get_y_summary(
                 df.loc[n, "tech_place"] = e
                 df.loc[n, "start_M"] = time[0]
                 df.loc[n, "end_M"] = time[1]
-                df.loc[n, "unified_name"] = description_dict[e]
+                df.loc[n, "unified_name"] = description_dictionary[e]
                 n += 1
 
     return df
+
+
+def get_unified_name_dictionary(i: int, unified_tech_places: pd.DataFrame) -> dict:
+    """
+    Generates dictionary {unified_name: description}
+    """
+    df = unified_tech_places[unified_tech_places["equipment"] == str(i)]
+    unified_name = df["unified_name"].tolist()
+    description = df["description"].tolist()
+    unified_name_dictionary = {k: v for (k, v) in zip(unified_name, description)}
+
+    return unified_name_dictionary
+
+
+def add_tech_place_description(
+    i: int, unified_name_dictionary: dict, df: pd.DataFrame
+) -> pd.DataFrame:
+    """
+    Adds technical place description based on unified_name and equipment number.
+    """
+    unified_names = df.loc[df["equipment"] == i, "unified_name"].unique().tolist()
+    cond_1 = df["equipment"] == i
+    for name in unified_names:
+        description = unified_name_dictionary[name]
+        cond_2 = df["unified_name"] == name
+        df.loc[cond_1 & cond_2, "description"] = description
+
+    return df
+
+
+def get_missing_labels(
+    y_summary: pd.DataFrame, messages: pd.DataFrame, unified_tech_places: pd.DataFrame
+) -> pd.DataFrame:
+    """
+    Selects labels, which were not reflected in y_train, from messages.
+    """
+    df_M3 = pd.DataFrame()
+    cols = [
+        "start_M",
+        "ДАТА_УСТРАНЕНИЯ_НЕИСПРАВНОСТИ",
+        "ВИД_СООБЩЕНИЯ",
+        "unified_name",
+    ]
+    cols_y = ["start_M", "tech_place"]
+    for i in range(4, 10):
+        a = y_summary.loc[y_summary["equipment"] == str(i), cols_y].set_index("start_M")
+        b = messages.loc[messages["equipment"] == str(i), cols].set_index("start_M")
+        df = pd.concat([a, b], axis=1)
+        df = df.loc[
+            df["tech_place"].isnull() & (df["ВИД_СООБЩЕНИЯ"] == "M3"), cols[1:]
+        ].reset_index()
+        df["equipment"] = i
+        df_M3 = pd.concat([df_M3, df], axis=0)
+
+        unified_name_dict = get_unified_name_dictionary(i, unified_tech_places)
+        df_M3 = add_tech_place_description(i, unified_name_dict, df_M3)
+
+    return df_M3.reset_index(drop=True)
+
+
+def add_missing_labels(
+    df_M3: pd.DataFrame,
+    path: Optional[str] = None,
+    folder: Optional[str] = None,
+):
+    """
+    Adds labels reflected only in messages to y_train.
+    """
+    if path is None:
+        path = PATH
+    if folder is None:
+        folder = FOLDER_2
+
+    for i in range(4, 10):
+        y = load_y(i, path)
+        tech_places = df_M3.loc[df_M3["equipment"] == i, "description"].unique()
+        for tech_place in tech_places:
+            anomalies = df_M3.loc[df_M3["description"] == tech_place, :]
+            for j in anomalies.index:
+                t1 = anomalies.loc[j, "start_M"]
+                t2 = anomalies.loc[j, "ДАТА_УСТРАНЕНИЯ_НЕИСПРАВНОСТИ"]
+                y.loc[t1:t2, tech_place] = 2
+        y.to_parquet(
+            path + folder + f"y{i}_updated.parquet",
+        )
