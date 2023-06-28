98f728d3bb8e3d0041bcd7663401fc84f1faf106
Yanina Kutovaya <kutovaiayp@yandex.ru>
2023-06-28 16:51:17 +0300

    Add ethalon_periods.py

---
+++ src/predictive_maintenance/data/ethalon_periods.py
@@ -0,0 +1,244 @@
+import logging
+import pandas as pd
+import numpy as np
+from typing import Optional
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+
+from .make_dataset import load_X
+import matplotlib.pyplot as plt
+
+
+logger = logging.getLogger(__name__)
+
+__all__ = ["generate_ethalon_dataset"]
+
+
+PATH = ""
+
+START = pd.Timestamp("2019-01-16 13:00:00")
+END = pd.Timestamp("2021-12-31 23:59:00")
+TIME_TO_STOPPAGE = pd.Timedelta(50, "H")
+TIME_FROM_STOPPAGE = pd.Timedelta(1, "H")
+INPUT_PERIOD = pd.Timedelta(23, "H")
+
+FREQUENCY = "1H"
+
+MAX_VIBRATION = 10
+MAX_TEMPERATURE = 80
+
+VIBRATION_COLUMNS = [
+    "ВИБРАЦИЯ НА ОПОРЕ 1",
+    "ВИБРАЦИЯ НА ОПОРЕ 2",
+    "ВИБРАЦИЯ НА ОПОРЕ 3",
+    "ВИБРАЦИЯ НА ОПОРЕ 3 ПРОДОЛЬНАЯ",
+    "ВИБРАЦИЯ НА ОПОРЕ 4",
+    "ВИБРАЦИЯ НА ОПОРЕ 4 ПРОДОЛЬНАЯ",
+]
+TEMPERATURE_COLUMNS = [
+    "ТЕМПЕРАТУРА МАСЛА В МАСЛОБЛОКЕ",
+    "ТЕМПЕРАТУРА МАСЛА В СИСТЕМЕ",
+    "ТЕМПЕРАТУРА ПОДШИПНИКА НА ОПОРЕ 1",
+    "ТЕМПЕРАТУРА ПОДШИПНИКА НА ОПОРЕ 2",
+    "ТЕМПЕРАТУРА ПОДШИПНИКА НА ОПОРЕ 3",
+    "ТЕМПЕРАТУРА ПОДШИПНИКА НА ОПОРЕ 4",
+]
+STOPPAGES = ["ТЕХНИЧЕСКИЕ НЕПЛАНОВЫЕ", "ТЕХНОЛОГИЧЕСКИЕ НЕПЛАНОВЫЕ"]
+
+PLOT = False
+
+
+def generate_ethalon_dataset(
+    messages,
+    path: Optional[str] = None,
+    freq: Optional[str] = None,
+    max_vibration: Optional[float] = None,
+    max_temperature: Optional[float] = None,
+    vibration_columns: Optional[list] = None,
+    temperature_columns: Optional[list] = None,
+    time_to_stoppage: Optional[pd.Timedelta] = None,
+    time_from_stoppage: Optional[pd.Timedelta] = None,
+    plot: Optional[bool] = None,
+):
+    """
+    Generates dataset for LSTM ethalon model.
+    """
+
+    if path is None:
+        path = PATH
+    if freq is None:
+        freq = FREQUENCY
+    if max_vibration is None:
+        max_vibration = MAX_VIBRATION
+    if max_temperature is None:
+        max_temperature = MAX_TEMPERATURE
+    if vibration_columns is None:
+        vibration_columns = VIBRATION_COLUMNS
+    if temperature_columns is None:
+        temperature_columns = TEMPERATURE_COLUMNS
+    if time_to_stoppage is None:
+        time_to_stoppage = TIME_TO_STOPPAGE
+    if time_from_stoppage is None:
+        time_from_stoppage = TIME_FROM_STOPPAGE
+    if plot is None:
+        plot = PLOT
+
+    ethalon_periods = select_ethalon_periods(
+        messages,
+        path,
+        freq,
+        max_vibration,
+        max_temperature,
+        vibration_columns,
+        temperature_columns,
+        time_to_stoppage,
+        time_from_stoppage,
+    )
+    df, scaler, pca = get_pca_components(ethalon_periods, plot)
+
+    t = pd.Timedelta(freq)
+    ethalon_dataset = []
+    for e in range(4, 10):
+        df_ = df[df["equipment"] == e]
+        t0 = df_.index[0]
+        for t1 in df_.index[1:]:
+            if t1 == t0 + time_to_stoppage:
+                ethalon_dataset.append(df_[t0 : t1 - t])
+                t0 += t
+            elif t1 > t0 + time_to_stoppage:
+                t0 = t1
+    ethalon_dataset = np.stack(ethalon_dataset, axis=0)[:, :, :-1]
+
+    return ethalon_dataset, scaler, pca
+
+
+def select_ethalon_periods(
+    messages,
+    path: Optional[str] = None,
+    freq: Optional[str] = None,
+    max_vibration: Optional[float] = None,
+    max_temperature: Optional[float] = None,
+    vibration_columns: Optional[list] = None,
+    temperature_columns: Optional[list] = None,
+    time_to_stoppage: Optional[pd.Timedelta] = None,
+    time_from_stoppage: Optional[pd.Timedelta] = None,
+):
+    """
+    Selects periods with vibration lower than max_vibration and temperature lower
+    than max_temperature.
+    All the stoppages and given periods before and after stoppages are excluded.
+    """
+    if path is None:
+        path = PATH
+    if freq is None:
+        freq = FREQUENCY
+    if max_vibration is None:
+        max_vibration = MAX_VIBRATION
+    if max_temperature is None:
+        max_temperature = MAX_TEMPERATURE
+    if vibration_columns is None:
+        vibration_columns = VIBRATION_COLUMNS
+    if temperature_columns is None:
+        temperature_columns = TEMPERATURE_COLUMNS
+    if time_to_stoppage is None:
+        time_to_stoppage = TIME_TO_STOPPAGE
+    if time_from_stoppage is None:
+        time_from_stoppage = TIME_FROM_STOPPAGE
+
+    messages = messages[messages["ДАТА_УСТРАНЕНИЯ_НЕИСПРАВНОСТИ"].notnull()]
+    ethalon_periods = pd.DataFrame()
+    for equipment in range(4, 10):
+        X = load_X(equipment, path).resample(freq).mean()
+
+        vibration_cols = [f"{str(equipment)} {i}" for i in vibration_columns]
+        temperature_cols = [f"{str(equipment)} {i}" for i in temperature_columns]
+
+        selected_periods = X[vibration_cols[0]] < max_vibration
+        for col in vibration_cols[1:]:
+            selected_periods &= X[col] < max_vibration
+        for col in temperature_cols:
+            selected_periods &= X[col] < max_temperature
+
+        df = messages[messages["equipment"] == str(equipment)]
+        for i in df.index:
+            t1 = df.loc[i, "ДАТА_НАЧАЛА_НЕИСПРАВНОСТИ"]
+            t2 = df.loc[i, "ДАТА_УСТРАНЕНИЯ_НЕИСПРАВНОСТИ"]
+
+            if df.loc[i, "ВИД_СООБЩЕНИЯ"] == "M1":
+                t1 -= time_to_stoppage
+                t2 += time_from_stoppage
+            selected_periods[t1:t2] = False
+
+        ind = X[selected_periods].index.tolist()
+        t0 = ind[0]
+        t_start = t0
+        t = pd.Timedelta(freq)
+
+        ethalon = pd.DataFrame()
+        for t1 in ind[1:]:
+            if t1 - t0 > t:
+                t_end = t0
+                if t_end - t_start >= time_to_stoppage:
+                    ethalon = pd.concat([ethalon, X[t_start:t_end]])
+                t_start = t1
+            t0 = t1
+
+        old_cols = ethalon.columns.tolist()
+        new_cols = [f"col_{i}" for i in range(ethalon.shape[1])]
+        ethalon.rename(
+            columns={old_cols[i]: new_cols[i] for i in range(len(old_cols))},
+            inplace=True,
+        )
+        ethalon["equipment"] = equipment
+
+        ethalon_periods = pd.concat([ethalon_periods, ethalon], axis=0)
+
+    return ethalon_periods
+
+
+def get_pca_components(
+    ethalon_periods,
+    plot: Optional[bool] = None,
+):
+    """
+    Applies StandardScaler to scale features for PCA.
+    Selects the number of components based on Kaiser's rule.
+    Generates new features baseed on PCA components.
+    """
+    if plot is None:
+        plot = PLOT
+
+    scaler = StandardScaler()
+    df_ = ethalon_periods.copy()
+    df_.iloc[:, :-1] = scaler.fit_transform(df_.iloc[:, :-1])
+    corr = df_.iloc[:, :-1].corr()
+    pca = PCA(n_components="mle")
+    pca.fit(corr)
+    n_components = (
+        pca.explained_variance_ratio_ > 1 / len(pca.explained_variance_ratio_)
+    ).sum()
+
+    if plot:
+        plot_explained_variance(pca, n_components)
+
+    pca = PCA(n_components=n_components)
+    pca.fit(corr)
+    df = pd.DataFrame(pca.transform(df_.iloc[:, :-1].fillna(0)), index=df_.index)
+    df["equipment"] = df_["equipment"]
+
+    return df, scaler, pca
+
+
+def plot_explained_variance(pca, n_components: int):
+    explained_variance = pca.explained_variance_ratio_[:n_components].sum().round(3)
+    comps = pca.explained_variance_ratio_[:n_components].round(3).tolist()
+
+    print(f"n_components = {n_components}")
+    print(f"explained_variance = {explained_variance}")
+    print(f"variance explained by {n_components} components: ", *comps, "\n")
+
+    plt.plot(pca.explained_variance_ratio_)
+    plt.title("Variance ratio explained by component")
+    plt.xlabel("The number of component")
+    plt.ylabel("Explained variance ratio")
