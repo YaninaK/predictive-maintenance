00aad4f2e41ee94974f9be44852722f8719e91e6
Yanina Kutovaya <kutovaiayp@yandex.ru>
2023-06-29 17:57:51 +0300

    Update train_test_datasets.py

--- src/predictive_maintenance/data/train_test_datasets.py
+++ src/predictive_maintenance/data/train_test_datasets.py
@@ -1,10 +1,51 @@
 import logging
 import pandas as pd
+import numpy as np
+from typing import Optional
+
+from .make_dataset import load_X
 
 
 logger = logging.getLogger(__name__)
 
 __all__ = ["make_test_dataset"]
+
+
+PATH = ""
+FREQUENCY = "1H"
+INPUT_PERIOD = pd.Timedelta(23, "H")
+
+
+def generate_test_dataset(
+    test_intervals: pd.DataFrame,
+    path: Optional[str] = None,
+    freq: Optional[str] = None,
+    input_period: Optional[pd.Timedelta] = None,
+) -> dict:
+    """
+    Generates test dataset for inference.
+    """
+    if path is None:
+        path = PATH
+    if freq is None:
+        freq = FREQUENCY
+    if input_period is None:
+        input_period = INPUT_PERIOD
+
+    all_test_periods = {}
+    for equipment in range(4, 10):
+        X_test = load_X(equipment, path=PATH, prefix="X_test").resample(freq).median()
+        test_periods = []
+
+        for t1 in test_intervals["start"]:
+            t0 = t1 - input_period
+            a = X_test.loc[t0:t1, :]
+            if a.shape[0] == input_period // pd.Timedelta(freq):
+                test_periods.append(X_test.loc[t0:t1, :].bfill().ffill())
+
+        all_test_periods[equipment] = np.array(test_periods)
+
+    return all_test_periods
 
 
 def combine_test_intervals(test_intervals: pd.DataFrame) -> pd.DataFrame:
