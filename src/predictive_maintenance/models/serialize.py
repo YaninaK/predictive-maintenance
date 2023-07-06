95b5edff1b44bcf61e0a5c40920710f4ec96f905
Yanina Kutovaya <kutovaiayp@yandex.ru>
2023-07-06 11:13:40 +0300

    Update serialize.py

--- src/predictive_maintenance/models/serialize.py
+++ src/predictive_maintenance/models/serialize.py
@@ -1,12 +1,53 @@
 import os
 import logging
 import tensorflow as tf
+import arviz as az
 import joblib
 
 
 logger = logging.getLogger()
 
 __all__ = ["store", "load"]
+
+
+def store_lstm(model, filename: str, path: str = "default"):
+    if path == "default":
+        path = models_path()
+
+    filepath = os.path.join(path, filename + ".h5")
+
+    logger.info(f"Dumpung LSTM model into {filepath}")
+    model.save(filepath)
+
+
+def load_lstm(filename: str, path: str = "default"):
+    if path == "default":
+        path = models_path()
+    filepath = os.path.join(path, filename + ".h5")
+
+    logger.info(f"Loading LSTM model from {filepath}")
+
+    return tf.keras.models.load_model(filepath)
+
+
+def store_bmb(model, filename: str, path: str = "default"):
+    if path == "default":
+        path = models_path()
+
+    filepath = os.path.join(path, filename + ".h5netcdf")
+
+    logger.info(f"Dumpung Bayesian model into {filepath}")
+    az.InferenceData.to_netcdf(model, filepath)
+
+
+def load_bmb(filename: str, path: str = "default"):
+    if path == "default":
+        path = models_path()
+    filepath = os.path.join(path, filename + ".h5netcdf")
+
+    logger.info(f"Loading Bayesian model from {filepath}")
+
+    return az.InferenceData.from_netcdf(filepath)
 
 
 def store(model, filename: str, path: str = "default"):
@@ -33,5 +74,5 @@ def models_path() -> str:
     script_path = os.path.abspath(__file__)
     script_dir_path = os.path.dirname(script_path)
     models_folder = os.path.join(script_dir_path, "..", "..", "..", "models")
-    
+
     return models_folder
