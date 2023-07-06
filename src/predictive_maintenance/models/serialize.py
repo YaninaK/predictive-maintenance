fd419dbb2f629228d80ecf8a49d1061a821b3ee6
Yanina Kutovaya <kutovaiayp@yandex.ru>
2023-07-06 14:04:42 +0300

    Update serialize.py

--- src/predictive_maintenance/models/serialize.py
+++ src/predictive_maintenance/models/serialize.py
@@ -3,6 +3,7 @@ import logging
 import tensorflow as tf
 import arviz as az
 import joblib
+from typing import Optional
 
 
 logger = logging.getLogger()
@@ -10,64 +11,53 @@ logger = logging.getLogger()
 __all__ = ["store", "load"]
 
 
-def store_lstm(model, filename: str, path: str = "default"):
+def store(
+    model,
+    filename: str,
+    path: str = "default",
+    model_type: Optional[str] = None,
+):
     if path == "default":
         path = models_path()
+    filepath = os.path.join(path, filename)
 
-    filepath = os.path.join(path, filename + ".h5")
+    if model_type == "tf":
+        filepath += ".h5"
+        logger.info(f"Dumping model into {filepath}")
+        model.save(filepath)
+    elif model_type == "bmb":
+        filepath += ".h5netcdf"
+        logger.info(f"Dumping model into {filepath}")
+        az.InferenceData.to_netcdf(model, filepath)
+    else:
+        filepath += ".joblib"
+        logger.info(f"Dumping model into {filepath}")
+        joblib.dump(model, filepath)
 
-    logger.info(f"Dumpung LSTM model into {filepath}")
-    model.save(filepath)
 
-
-def load_lstm(filename: str, path: str = "default"):
+def load(
+    filename: str,
+    path: str = "default",
+    model_type: Optional[str] = None,
+):
     if path == "default":
         path = models_path()
-    filepath = os.path.join(path, filename + ".h5")
+    filepath = os.path.join(path, filename)
 
-    logger.info(f"Loading LSTM model from {filepath}")
+    if model_type == "tf":
+        filepath += ".h5"
+        logger.info(f"Loading model from {filepath}")
+        return tf.keras.models.load_model(filepath)
 
-    return tf.keras.models.load_model(filepath)
+    elif model_type == "bmb":
+        filepath += ".h5netcdf"
+        logger.info(f"Loading model from {filepath}")
+        return az.InferenceData.from_netcdf(filepath)
 
-
-def store_bmb(model, filename: str, path: str = "default"):
-    if path == "default":
-        path = models_path()
-
-    filepath = os.path.join(path, filename + ".h5netcdf")
-
-    logger.info(f"Dumpung Bayesian model into {filepath}")
-    az.InferenceData.to_netcdf(model, filepath)
-
-
-def load_bmb(filename: str, path: str = "default"):
-    if path == "default":
-        path = models_path()
-    filepath = os.path.join(path, filename + ".h5netcdf")
-
-    logger.info(f"Loading Bayesian model from {filepath}")
-
-    return az.InferenceData.from_netcdf(filepath)
-
-
-def store(model, filename: str, path: str = "default"):
-    if path == "default":
-        path = models_path()
-
-    filepath = os.path.join(path, filename + ".joblib")
-
-    logger.info(f"Dumpung model into {filepath}")
-    joblib.dump(model, filepath)
-
-
-def load(filename: str, path: str = "default"):
-    if path == "default":
-        path = models_path()
-    filepath = os.path.join(path, filename + ".joblib")
-
-    logger.info(f"Loading model from {filepath}")
-
-    return joblib.load(filepath)
+    else:
+        filepath += ".joblib"
+        logger.info(f"Loading model from {filepath}")
+        return joblib.load(filepath)
 
 
 def models_path() -> str:
